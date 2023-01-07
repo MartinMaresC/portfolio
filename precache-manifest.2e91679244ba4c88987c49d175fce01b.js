9ff411e159997fb817b0f73edab7e1e08352f99e
Martin <martinmarescr@gmail.com>
2023-01-07 04:29:21 -0600

    Updates

---
+++ precache-manifest.2e91679244ba4c88987c49d175fce01b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "eeafb538c9dfcfd8a7ce",
+    "url": "/portfolio/static/css/main.9257fd36.chunk.css"
+  },
+  {
+    "revision": "eeafb538c9dfcfd8a7ce",
+    "url": "/portfolio/static/js/main.eeafb538.chunk.js"
+  },
+  {
+    "revision": "2417dde256020b67065a",
+    "url": "/portfolio/static/js/1.2417dde2.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "ba31af82e19dee5b0dd14a519352a2dc",
+    "url": "/portfolio/index.html"
+  }
+];
